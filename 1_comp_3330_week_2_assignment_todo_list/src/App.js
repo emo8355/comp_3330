43d99334e516ec613247e6c4ac334acdc4964e46
homer li <homer.lee.0508@gmail.com>
2020-09-18 18:22:48 -0700

    week 2

--- 1_comp_3330_week_2_assignment_todo_list/src/App.js
+++ 1_comp_3330_week_2_assignment_todo_list/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from "react";
 import "./App.css";
 import Todos from "./components/Todos";
+import FilterButton from "./components/FilterButton";
 
 function App() {
 	//useState hook init
@@ -9,6 +10,8 @@ function App() {
 		{ id: 1, task: "Sleep", isChecked: false },
 		{ id: 2, task: "Repeat", isChecked: false },
 	]);
+	const [filter, setFilter] = useState("All");
+	const [newName, setNewName] = useState("");
 
 	//useRef init
 	const newTask = useRef();
@@ -23,7 +26,6 @@ function App() {
 		}
 		newTask.current.value = null;
 	};
-	const editTodo = (e) => {};
 
 	function deleteTodo(id) {
 		const newTodo = [...todos].filter((todo) => todo.id !== id);
@@ -36,33 +38,56 @@ function App() {
 		setTodos([...todos]);
 	}
 
-	console.log(todos);
+	const editTodo = (id, newTask) => {
+		const editedTask = [...todos].find((todo) => todo.id === id);
+		editedTask.task = newTask;
+		setNewName(editedTask);
+	};
+
+	const FILTER_MAP = {
+		All: () => true,
+		Active: (task) => !task.isChecked,
+		Completed: (task) => task.isChecked,
+	};
+
+	const FILTER_NAMES = Object.keys(FILTER_MAP);
+	const filterList = FILTER_NAMES.map((name) => (
+		<FilterButton
+			key={name}
+			name={name}
+			isPressed={name === filter}
+			setFilter={setFilter}
+		/>
+	));
+
+	const taskList = todos
+		.filter((task) => FILTER_MAP[filter](task))
+		.map((todo) => (
+			<Todos
+				key={todo.id}
+				todo={todo}
+				editTodo={editTodo}
+				deleteTodo={deleteTodo}
+				isComplete={isComplete}
+			/>
+		));
+
 	const remainTasks = todos.filter((todo) => !todo.isChecked).length;
 	return (
 		<div className="App">
-			<h1>Todo List</h1>
-			<h3>What needs to be done ? </h3>
+			<br />
+			<h2>What needs to be done ? </h2>
 			<div className="taskForm" autoComplete="off">
 				<input placeholder="Enter your task" ref={newTask} type="text"></input>
 				&nbsp;
 				<button onClick={addTodo}>Add</button>
 			</div>
 			<br />
-			<div className="listControl">
-				<button>Show all tasks</button>
-				&nbsp;
-				<button>Show active tasks</button>
-				&nbsp;
-				<button>Show completed tasks</button>
+			<div className="listControl">{filterList}</div>
+			<div className="tasks_bdy">
+				<h1>{remainTasks} Tasks Remaining</h1>
+				{taskList}
 			</div>
-			<h2>{remainTasks} tasks remaining</h2>
-			<ul>
-				<Todos
-					todolist={todos}
-					deleteTodo={deleteTodo}
-					isComplete={isComplete}
-				/>
-			</ul>
 		</div>
 	);
 }
--- 1_comp_3330_week_2_assignment_todo_list/src/components/Todos.js
+++ 1_comp_3330_week_2_assignment_todo_list/src/components/Todos.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
-export default function Todos({ todolist, deleteTodo, isComplete }) {
+export default function Todos({ todo, deleteTodo, isComplete, editTodo }) {
+	const [isEditing, setEditing] = useState(false);
+
+	const editTask = useRef();
 	const handleDelete = (id) => {
 		deleteTodo(id);
 	};
@@ -9,9 +12,37 @@ export default function Todos({ todolist, deleteTodo, isComplete }) {
 		isComplete(id);
 	};
 
-	return todolist.map((todo) => {
-		return (
-			<li key={todo.id}>
+	const handleEdit = (id) => {
+		editTodo(id, editTask.current.value);
+		setEditing(false);
+	};
+
+	const editMode = (
+		<div>
+			<div>
+				<label htmlFor={todo.id}></label>
+				<input
+					id={todo.id}
+					className="newTaskInput"
+					placeholder="Enter New Task"
+					type="text"
+					ref={editTask}
+				/>
+			</div>
+			<div className="singleTaskControl">
+				<button className="cancel_btn" onClick={() => setEditing(false)}>
+					Cancel
+				</button>
+				<button className="save_btn" onClick={() => handleEdit(todo.id)}>
+					Save
+				</button>
+			</div>
+		</div>
+	);
+
+	const viewMode = (
+		<div>
+			<div className="taskSection">
 				<input
 					type="checkbox"
 					checked={todo.isChecked}
@@ -19,19 +50,31 @@ export default function Todos({ todolist, deleteTodo, isComplete }) {
 						handleComplete(todo.id);
 					}}
 				></input>
-				<span>{todo.task}</span>1
-				<div className="singleTaskControl">
-					<button>Edit {todo.task}</button>&nbsp;
-					<button
-						onClick={() => {
-							handleDelete(todo.id);
-						}}
-					>
-						Delete {todo.task}
-					</button>
-				</div>
-				<br />
-			</li>
-		);
-	});
+				&nbsp;&nbsp;
+				<span className="inputBox">{todo.task}</span>
+			</div>
+			<div className="singleTaskControl">
+				<button className="edi_btn" onClick={() => setEditing(true)}>
+					Edit {todo.task}
+				</button>
+				&nbsp;
+				<button
+					className="del_btn"
+					onClick={() => {
+						handleDelete(todo.id);
+					}}
+				>
+					Delete {todo.task}
+				</button>
+			</div>
+		</div>
+	);
+
+	return (
+		<div key={todo.id} className="task">
+			{isEditing ? editMode : viewMode}
+
+			<br />
+		</div>
+	);
 }
